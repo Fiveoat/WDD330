750b6402ccbb98db551ae4fc2ad6389d8b596996
mountain <Fiveoat@gmail.com>
2021-01-03 23:47:13 -0700

    more changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-let weekList = ['Week1', 'Week2', 'Week3', 'Week4', 'Week5', 'Week6', 'Week7', 'Week8', 'Week9', 'Week10', 'Week11', 'Week12', 'Week13', 'Week14']
-let weekDirs = ["weeks/week1/index.html", "weeks/week2/index.html", "weeks/week3/index.html", "weeks/week4/index.html", "weeks/week5/index.html", "weeks/week6/index.html", "weeks/week7/index.html", "weeks/week8/index.html", "weeks/week9/index.html", "weeks/week10/index.html", "weeks/week11/index.html", "weeks/week12/index.html", "weeks/week13/index.html", "weeks/week14/index.html"];
+const links = []
+for (i=1; i <14; i++){
+    links.push({label: 'Week' + i.toString(), url: "weeks/week" + i.toString() + "/index.html"})
+}
 const assignmentList = document.querySelector(".assignment-list");
-for (i = 0; i < weekList.length; i++) {
+for (i = 0; i < links.length; i++) {
     var weekLine = document.createElement("li");
     var a = document.createElement("a");
-    var link = document.createTextNode(weekList[i]);
+    var link = document.createTextNode(links[i].label);
     weekLine.classList.add("weeks");
-    a.title = weekList[i];
-    a.href = weekDirs[i];
+    a.title = links[i].label;
+    a.href = links[i].url;
     a.appendChild(link);
     weekLine.appendChild(a);
     assignmentList.appendChild(weekLine);
